97dd0b260907c49dae96f1a85625e746c5f45da9
Abhishekh Reddy <helloarm@pm.me>
2026-05-14 16:13:50 -0700

    Fix linting issue
    Signed-off-by: Abhishekh Reddy <helloarm@pm.me>

--- honeybee_watchdogs/honeybee_watchdogs/power_off_backpack.py
+++ honeybee_watchdogs/honeybee_watchdogs/power_off_backpack.py
@@ -44,7 +44,6 @@ class PowerOffBackPack(Node):
             if self.poweroff_count >= 20:  # Hold down for 2-3s
                 print('Power off event detected. Shutting down backpack.')
                 os.system('sudo poweroff')  # Make sure to add the user to sudoers with NOPASSWD
-                                            # for this command
         else:
             self.poweroff_count = 0
 
